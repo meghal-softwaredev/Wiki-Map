8978823f498b8339a1a9b21ffa726bcd5f8d81d0
Meghal <meghal.shah210@gmail.com>
2021-10-16 13:16:06 -0400

    Initial setup of home page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,22 @@
 // Client facing scripts here
+$(() => {
+  const $main = $('#main-content');
+  const $logInForm = $(`
+  <form id="login-form" class="login-form">
+      <p>Login</p>
+      <div class="login-form__field-wrapper">
+        <input type="email" name="email" placeholder="Email">
+      </div>
+
+      <div class="login-form__field-wrapper">
+          <input type="password" name="password" placeholder="Password">
+        </div>
+
+      <div class="login-form__field-wrapper">
+          <button>Login</button>
+          <a id="login-form__cancel" href="#">Cancel</a>
+      </div>
+    </form>
+  `);
+  $logInForm.appendTo($main);
+});
