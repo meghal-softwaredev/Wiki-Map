17593b6fe6d9e5dd4a78067102517372c3092d91
Adriana <adriana.calvomatos@gmail.com>
2021-10-16 20:31:08 +0000

    clean up styles and watch sass

--- public/scripts/components/header.js
+++ public/scripts/components/header.js
@@ -7,7 +7,7 @@ $(() => {
   <nav id="navbar">
     <div class="nav-logo">Wiki Maps</div>
     <ul class="nav-links">
-      <li class="login_button">Log In</li>
+      <li class="login_button">Login</li>
       <li class="sign-up_button">Sign Up</li>
     </ul>
   </nav>
@@ -24,11 +24,3 @@ $(() => {
     views_manager.show("signUp");
   });
 });
-
-// <nav id="page-header__user-links" class="page-header__user-links">
-//   <ul>
-//     <li class="map">Logo</li>
-//     <li class="login_button">Log In</li>
-//     <li class="sign-up_button">Sign Up</li>
-//   </ul>
-// </nav>;
--- public/scripts/components/register.js
+++ public/scripts/components/register.js
@@ -41,20 +41,3 @@ $(() => {
   //     });
   // });
 });
-
-{
-  /* <form id="register-form" class="register-form">
-<p>Register</p>
-
-<div class="register-form__field-wrapper">
-  <input type="name" name="name" placeholder="Name">
-</div>
-<div class="register-form__field-wrapper">
-  <input type="email" name="email" placeholder="Email">
-</div>
-
-<div class="register-form__field-wrapper">
-    <input type="password" name="password" placeholder="Password">
-  </div>
-</form> */
-}
