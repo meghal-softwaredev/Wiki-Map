9f9fdf0caacb8906a81086283dc8e7d8a46647fa
Meghal <meghal.shah210@gmail.com>
2021-10-16 15:33:42 -0400

    add login form

---
+++ public/scripts/components/login.js
@@ -0,0 +1,26 @@
+// Client facing scripts here
+$(() => {
+  const $main = $('#main-content');
+  const $logInForm = $(`
+  <div class="container login-container">
+    <div class="img">
+      <img src='../../assets/map.png' alt='image' height=400px width=400px>
+    </div>
+    <div class="login">
+    <h1 class="ml-3">Login</h1>
+    <form method="POST" action="/login">
+      <div class="form-group ml-3">
+        <label for="email">Email address</label>
+          <input type="email" class="form-control" style="width:300px;" id="email" name="email" placeholder="Enter email">
+      </div>
+      <div class="form-group ml-3">
+        <label for="password">Password</label>
+          <input type="password" class="form-control" style="width:300px;" id="password" name="password" placeholder="Password">
+      </div>
+      <button type="submit" class="btn btn-primary ml-3">Login</button>
+    </form>
+    </div>
+  </div>
+  `);
+  $logInForm.appendTo($main);
+});
