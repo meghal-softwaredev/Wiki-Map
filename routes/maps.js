49d92c05c76331edd9aaeaf2da92d80d240e92dc
Adriana <adriana.calvomatos@gmail.com>
2021-10-17 11:29:06 +0000

    update maps.js

--- routes/maps.js
+++ routes/maps.js
@@ -1,7 +1,7 @@
 /*
  * All routes for Widgets are defined here
- * Since this file is loaded in server.js into api/maps,
- *   these routes are mounted onto /maps
+ * Since this file is loaded in server.js into api/widgets,
+ *   these routes are mounted onto /widgets
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
@@ -9,7 +9,6 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-<<<<<<< HEAD
   // create a new map
   router.post("/new", (req, res) => {
     console.log(req.body);
@@ -23,23 +22,11 @@ module.exports = (db) => {
       RETURNING *`,
         [title, description]
       )
-=======
-  // create a new map in database and adding contributors if there is one.
-  router.post('/new', (req, res) => {
-    const user = req.body;
-    const {title, description, owner_id} = user;
-    return db.query(`
-      INSERT INTO maps (title, description, owner_id)
-      VALUES ($1, $2, $3)
-      RETURNING *`,
-      [title, description, owner_id])
->>>>>>> 22f43c96211277e3e10d666fa6d803db38988938
       .then((result) => {
         console.log("result", result.rows[0]);
         return res.json({ user: result.rows[0] });
       })
       .catch((err) => err.message);
-<<<<<<< HEAD
   });
 
   router.get("/all", (req, res) => {
@@ -54,7 +41,6 @@ module.exports = (db) => {
         })
         .catch((err) => err.message);
     }
-
     return (
       db
         ///////////// add params back in after done testing $1 [userID]
@@ -71,8 +57,3 @@ module.exports = (db) => {
 
   return router;
 };
-=======
-    });
-    return router;
-}
->>>>>>> 22f43c96211277e3e10d666fa6d803db38988938
