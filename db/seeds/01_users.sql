a07fdd82c3c8a817f6665242cf8d111e2a9428a2
Adriana <adriana.calvomatos@gmail.com>
2021-10-20 01:53:32 +0000

    update favourites table in db to remove boolean

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -7,8 +7,8 @@ VALUES (1, 'parks', 'green places'), (2, 'restaurants', 'best lunch spots'), (2,
 INSERT INTO contributors (user_id, map_id)
 VALUES (1, 2);
 
-INSERT INTO favourites (user_id, map_id, favourite)
-VALUES (1, 2, true), (1, 1, true), (2, 2, true), (2, 1, true);
+INSERT INTO favourites (user_id, map_id)
+VALUES (1, 2), (1, 1), (2, 2), (2, 1);
 
 INSERT INTO points (user_id, map_id, title, description, img_url)
 VALUES (1, 2, 'tim hortons', 'coffee and timbits', 'https://imgur.com/gallery/rcxKimS')
